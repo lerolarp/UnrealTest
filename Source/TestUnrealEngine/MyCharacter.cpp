a081c9214e098b52460c743a49b0890419994555
lerolarp <lerolarp@gmail.com>
2022-11-07 21:06:33 +0900

    11/07 :  아이템 줍기

--- Source/TestUnrealEngine/MyCharacter.cpp
+++ Source/TestUnrealEngine/MyCharacter.cpp
@@ -65,11 +65,9 @@ void AMyCharacter::BeginPlay()
 	if (CurrentWeapon)
 	{
 
-		CurrentWeapon->AttachToComponent(GetMesh(),
-			FAttachmentTransformRules::SnapToTargetNotIncludingScale,
-			WeaponSocket);
-
-
+		//CurrentWeapon->AttachToComponent(GetMesh(),
+		//	FAttachmentTransformRules::SnapToTargetNotIncludingScale,
+		//	WeaponSocket);
 	}
 
 }
