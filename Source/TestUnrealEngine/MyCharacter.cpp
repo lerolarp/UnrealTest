ec5340a1f2dc135e7f68a6dcb741f874575cf29f
lerolarp <lerolarp@gmail.com>
2022-11-14 23:09:02 +0900

    11/14 : AIController

--- Source/TestUnrealEngine/MyCharacter.cpp
+++ Source/TestUnrealEngine/MyCharacter.cpp
@@ -11,6 +11,7 @@
 #include "MyStatComponent.h"
 #include "Components/WidgetComponent.h"
 #include "MyCharacterWidget.h"
+#include "MyAIController.h"
 
 
 // Sets default values
@@ -53,6 +54,9 @@ AMyCharacter::AMyCharacter()
 		HpBar->SetWidgetClass(UW.Class);
 		HpBar->SetDrawSize(FVector2D(200.f, 50.f));
 	}
+
+	AIControllerClass = AMyAIController::StaticClass();
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 }
 
 // Called when the game starts or when spawned
