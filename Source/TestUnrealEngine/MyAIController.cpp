309e588a144878cd977b0773a1e0074de0a937ff
lerolarp <lerolarp@gmail.com>
2022-11-23 22:46:30 +0900

    11/23 : BehaviorTree #1

--- Source/TestUnrealEngine/MyAIController.cpp
+++ Source/TestUnrealEngine/MyAIController.cpp
@@ -4,18 +4,39 @@
 #include "MyAIController.h"
 #include "NavigationSystem.h"
 #include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 AMyAIController::AMyAIController()
 {
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BT(TEXT("BehaviorTree'/Game/AI/BT_MyCharacter.BT_MyCharacter'"));
+	if (BT.Succeeded())
+	{
+		BehaviorTree = BT.Object;
+	}
 
+	static ConstructorHelpers::FObjectFinder<UBlackboardData> BD(TEXT("BlackboardData'/Game/AI/BB_MyCharacter.BB_MyCharacter'"));
+	if (BD.Succeeded())
+	{
+		BlackboardData = BD.Object;
+	}
+
+	
 }
 
 void AMyAIController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
+	 
+	//GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AMyAIController::RandomMove, 3.f, true);
+	if (UseBlackboard(BlackboardData, Blackboard))
+	{
+		if (RunBehaviorTree(BehaviorTree))
+		{
 
-	GetWorld()->GetTimerManager().
-		SetTimer(TimerHandle, this, &AMyAIController::RandomMove, 3.f, true);
+		}
+	}
 }
 
 void AMyAIController::OnUnPossess()
